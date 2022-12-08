137bc6daa934cc2c593e8806de52727595ddde3b
gwarang <hrgoh.kcs@gmail.com>
2022-12-08 15:09:34 +0900

    edit README file & API request address

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 export function getPhones(){
-    // return axios.get('http://127.0.0.1:5000/phones')
-    return axios.get('https://oziy2g9zm9.execute-api.ap-northeast-1.amazonaws.com/dev/phones')
+    return axios.get('http://127.0.0.1:5000/phones')
+    // return axios.get('[your API Gateway address]/[zappa stage name]/phones')
 }
 
 export function addPhone(param) {
     return axios({
         method: 'post',
-        // url : 'http://127.0.0.1:5000/addPhone',
-        url : 'https://oziy2g9zm9.execute-api.ap-northeast-1.amazonaws.com/dev/addPhone',
+        url : 'http://127.0.0.1:5000/addPhone',
+        // url : '[your API Gateway address]/[zappa stage name]/addPhone',
         
         data: param
     })
@@ -18,8 +18,8 @@ export function addPhone(param) {
 export function deletePhone(param) {
     return axios({
         method: 'post',
-        // url : 'http://127.0.0.1:5000/deletePhone',
-        url : 'https://oziy2g9zm9.execute-api.ap-northeast-1.amazonaws.com/dev/deletePhone',
+        url : 'http://127.0.0.1:5000/deletePhone',
+        // url : '[your API Gateway address]/[zappa stage name]/deletePhone',
         data: param
     })
 }
@@ -27,8 +27,8 @@ export function deletePhone(param) {
 export function updatePhone(param) {
     return axios({
         method: 'post',
-        // url : 'http://127.0.0.1:5000/updatePhone',
-        url : 'https://oziy2g9zm9.execute-api.ap-northeast-1.amazonaws.com/dev/updatePhone',
+        url : 'http://127.0.0.1:5000/updatePhone',
+        // url : '[your API Gateway address]/[zappa stage name]/updatePhone',
         data: param
     })
 }
